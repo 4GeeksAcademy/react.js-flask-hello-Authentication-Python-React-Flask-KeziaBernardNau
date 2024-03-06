f017d7194b5fe5d94b36c70047fa80951c387823
Kezia Bernard-Nau <147874734+KeziaBernardNau@users.noreply.github.com>
2024-03-06 17:54:10 +0000

    flask

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -13,14 +13,85 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			user: null,
+			token: null
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
+			// create a sign up function here that makes a (POST) fetch request to /sign in your backend
+			// then you will need to make a button the nav bar to take you a to a sign up page
+			// make the sign up page call this sign up function to create a user
+			sync_Session_Token: () =>{
+				const token= sessionStorage.getItem("token");
+				if(token && token !== "" && token !== undefined){
+					setStore({token:token})
+				}
+			},
+			
+			signup: async(email, password) => {
+				let response = await fetch(process.env.BACKEND_URL + "/api/signup", 
+				{
+					method: "POST",
+					headers: {
+					  "Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+					  email: email,
+					  password: password,
+					}),
+				})
+				let data = await response.json()
+				setStore({user: data})
+			},	
+			
+			login:async(email, password) => {
+				const options = {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(
+						{
+							email:email, 
+							password:password
+						}
+					)
+				}
+				try{
+					const response=await fetch(process.env.BACKEND_URL+"/api/token", options)
+					if (response.status !== 200){
+						alert("Error response code:", response.status)
+						return false;
+					}
+					const data=await response.json()
+					console.log("access token:", data)
+					sessionStorage.setItem("token", data.access_token)
+					setStore({token: data.access_token})
+					return true
+				}
+				catch(error){
+					console.log("Login error, please try again.")
+				}
+			},
+
+			logout: () => {
+				sessionStorage.removeItem("token")
+				console.log("You are logged out.")
+				setStore({
+					token: null
+				})
+			}
+			// you will also need to create a login function that you will use the /token route for
+			// again you will need a button and page for this
+
+			// lastly you need to create a private/profile page.
+			// ask me for further detail when you get there
+			
+			
+			
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-
 			getMessage: async () => {
 				try{
 					// fetching data from the backend
